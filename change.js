3793a0a6060d3495976dbdc868f5fc48c9364a5e
Bharatkumar Kulkarni <bharatkumak@birlasoft.com>
2025-01-27 22:19:09 +0530

    changed

---
+++ change.js
@@ -0,0 +1,7 @@
+function change() {
+    var text = document.getElementById('text');
+    var newtext = prompt("enter new text");
+    var cur_date = new Date();
+    text.innerHTML = newtext + " " + cur_date;
+    text.style.fontSize = "13px";
+}
